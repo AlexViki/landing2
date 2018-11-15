2e846c116f4c930bd643c35e5aee3013865b45ee
Alex Viki <shevashokc@gmail.com>
2018-11-15 20:50:22 +0200

    Added js-script for mobile menu

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,6 +1,15 @@
 function showMenu() {
     var menu = document.getElementById('menu');
-    console.log(menu);
+    var menuStatus = menu.classList.contains('menu_state_open');
+    var menuStateOpen = 'menu_state_open';
+    if (menuStatus) {
+        console.log('Yes');
+    } else {
+        console.log('No');;
+    };
+    menu.classList.toggle(menuStateOpen);
 };
 
-//showMenu();
+var btnMenu = document.getElementById('menu-mobile-btn');
+
+btnMenu.onclick = showMenu;
