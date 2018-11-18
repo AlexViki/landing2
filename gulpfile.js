b258f5fe2fbe06c765c4415bc78fd9967d0543b5
Alex Viki <shevashokc@gmail.com>
2018-11-18 22:19:54 +0200

    working with configure file gulpfile (files less)

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-var gulp        = require('gulp');
-var include     = require('gulp-file-include');
-var rename      = require('gulp-rename');
+var gulp            = require('gulp');
+var include         = require('gulp-file-include');
+var rename          = require('gulp-rename');
+var less            = require('gulp-less');
+var autoprefixer    = require('gulp-autoprefixer');
+
+/* ************************************** */
 
 gulp.task('html', function(){
-    gulp.src('html/main.html')
+    return gulp.src('html/main.html')
     .pipe(include({
         prefix: '@@',
         basepath: '@file'
@@ -11,6 +15,15 @@ gulp.task('html', function(){
     .pipe(rename('/index.html'))
     .pipe(gulp.dest('./'))
 });
+
+gulp.task('less', function(){
+    gulp.src('/less/main.less')
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('/css'))
+});
+
+/* ************************************** */
 
 gulp.task('watch', function() {
     gulp.watch('html/**/*.html', ['html']);
