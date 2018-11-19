f21bfdfc0d046b19edf8eeffb73c2592958c038b
Alex Viki <shevashokc@gmail.com>
2018-11-19 21:48:46 +0200

    created task "css-libs". Modified all project

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,45 @@ var include         = require('gulp-file-include');
 var rename          = require('gulp-rename');
 var less            = require('gulp-less');
 var autoprefixer    = require('gulp-autoprefixer');
+var cssmin          = require('gulp-cssmin');
+cssnano             = require('gulp-cssnano');
 
 /* ************************************** */
 
 gulp.task('html', function(){
-    return gulp.src('html/main.html')
+    return gulp.src('src/html/main.html')
     .pipe(include({
         prefix: '@@',
         basepath: '@file'
     }))
     .pipe(rename('/index.html'))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('build'))
 });
 
-gulp.task('less', function(){
-    gulp.src('/less/main.less')
+gulp.task('css', function(){
+    return gulp.src('styles/less/main.less')
     .pipe(less())
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('/css'))
+    .pipe(autoprefixer({
+        browsers: ['last 5 versions'],
+        cascade: false
+    }))
+    .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css'))
+});
+
+gulp.task('css-libs', function(){
+    return gulp.src('styles/libs/*.css')
+    .pipe(cssnano())
+    .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css'))
 });
 
 /* ************************************** */
 
 gulp.task('watch', function() {
     gulp.watch('html/**/*.html', ['html']);
+    gulp.watch('styles/**/*.less', ['css']);
+    gulp.watch('styles/**/*.scc', ['css-libs']);
 });
